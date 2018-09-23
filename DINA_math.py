ce46f62e4844de4c36682495b4eaf6d41d85935c
yzd <412299369@qq.com>
2018-09-23 18:54:08 +0800

    update quick

--- DINA_math.py
+++ DINA_math.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import time
 import math
+from operator import mul
+from functools import reduce
 '''
 使用公开数据集，math2015中的FrcSub
 将data和q矩阵文件txt转为了csv的格式方便读取
@@ -10,7 +12,8 @@ import math
 # 用来测试少量的数据，减少计算等待时间
 headNum = 536
 # sg迭代的阈值
-threshold = 0.00001
+threshold = 0.001
+
 
 
 
@@ -39,6 +42,7 @@ def nTrueOrFalse(qk, l):
 拿训练集数据，80%的学生
 计算每道题目的s失误率和g猜测率
 '''
+
 def trainDINAModel():
     startTime = time.time()
     print('训练模型开始时间：'+str(int(startTime)))
@@ -46,19 +50,31 @@ def trainDINAModel():
     # 可以使用head函数控制读取的学生数量
     # sample表示采样，frac=0.8表示随机采样80%的记录
     # n 表示每个学生每道题目的答题情况，1表示答对，0表示答错
-    # n = pd.read_csv('math2015/FrcSub/data.csv').sample(frac=0.8)
-    n = pd.read_csv('math2015/FrcSub/data.csv').head(headNum).sample(frac=0.8)
+    n = pd.read_csv('math2015/Math1/data.csv').head(headNum).sample(frac=0.8).values
+    # n = pd.read_csv('math2015/FrcSub/data.csv').sample(frac=0.8).values
     # Q 表示每道题目答对需要掌握的知识点向量
-    Q = pd.read_csv('math2015/FrcSub/q.csv')
-
+    # Q = np.mat(pd.read_csv('math2015/FrcSub/q.csv'))
+    Q = np.mat(pd.read_csv('math2015/Math1/q.csv').head(15).values)
     # 536*0.8个学生,20道题目
     ni, nj = n.shape
     # 20道题目，8个知识点
     Qi, Qj = Q.shape
 
+    k = Qj
     # 计算每道题目的s失误率和g猜测率
     sg = np.zeros((nj, 2))
     # 计算每个学生，在2^k中技能模式下面的似然函数，暂且将似然函数等于后验概率
+
+
+    #构造K矩阵，表示k个技能可以组成的技能模式矩阵
+    K = np.mat(np.zeros((k, 2 ** k), dtype=int))
+    for j in range(2 ** k):
+        l = list(bin(j).replace('0b', ''))
+        for i in range(len(l)):
+            K[k - len(l) + i, j] = l[i]
+    #r矩阵表示理论上j这道题目对于l这个模式能否做对
+    std = np.sum(Q, axis=1)
+    r = (Q * K == std) * 1
 
 
     # 初始化每道题目的s失误率和g猜测率，
@@ -70,6 +86,7 @@ def trainDINAModel():
     continueSG = True
     kk =1
     # 计算s和g迭代的次数
+    # 学生*模式数 = 学生*  题目数         题目数*技能         技能*模式数
     while continueSG == True:
         # E步，求似然矩阵
         IL = np.ones((ni, 2 ** Qj))
@@ -77,77 +94,44 @@ def trainDINAModel():
         for l in range(2 ** Qj):
             # 学生的数量
             for i in range(ni):
-                # 题目的数量
-                for j in range(nj):
-                    # 计算Q矩阵中，第k题的情况下，l这个技能模式能否答对
-                    nl = nTrueOrFalse((Q.iloc[j]), l)
-                    # 理论上，答对的情况
-                    if nl == 1:
-                        # 实际该学生，对于k这道题做对了，似然函数*(1-s)
-                        if n.iloc[i][j] == 1:
-                            IL[i][l] *= (1 - sg[j][0])
-                        # 实际该学生，对于k这道题居然做错了，似然函数*s，失误率
-                        else:
-                            IL[i][l] *= sg[j][0]
-                    # 理论上，答错的情况
-                    else:
-                        # 实际该学生，对于k这道题居然做对了，似然函数*g，猜测率
-                        if n.iloc[i][j] == 1:
-                            IL[i][l] *= sg[j][1]
-                        # 实际该学生，对于k这道题确实做错了，似然函数*(1-g)
-                        else:
-                            IL[i][l] *= 1 - sg[j][1]
-            print('l总数:'+str(2 ** Qj)+',i总数:'+str(ni)+',l:'+str(l))
+                # 将全部题目的似然概率相乘  得到i学生对于l模式的后验概率，
+                # r.T[l].tolist()[0]表示理论情况与n[i]实际情况结合选择该道题目的似然概率
+                ll = map(lambda nl, nk, s, g: ((1 - s)**nk*s**(1-nk))**nl *(g**nk*(1 - g)**(1-nk))**(1-nl),
+                        n[i] , r.T[l].tolist()[0], sg[:,0], sg[:,1])
+                IL[i][l] = reduce(mul, ll)
         print("IL是训练集学生，所有技能模式的似然概率矩阵")
         print(IL)
 
-
-
         # M步，求s，g
-        # k表示每一道题目
-        for j in range(nj):
-            # 根据这四个参数来更新迭代s和g
-            I0 = 0
-            R0 = 0
-            I1 = 0
-            R1 = 0
-            # l表示每一种技能模式
-            for l in range(2 ** Qj):
+        # 根据这四个参数来更新迭代s和g
+        I0 = np.zeros(nj)
+        R0 = np.zeros(nj)
+        I1 = np.zeros(nj)
+        R1 = np.zeros(nj)
+        # l表示每一种技能模式
+        for l in range(2 ** Qj):
+            # i表示每一个学生
+            for i in range(ni):
+                # I1,R1的情况，表示理论上能做对
 
-                # 也需要计算k这道题，l这模式能否做对
-                nl = nTrueOrFalse((Q.iloc[j]), l)
-                # i表示每一个学生
-                for i in range(ni):
-                    # I1,R1的情况，表示理论上能做对
-                    if nl ==1:
-                        I1 += IL[i][l]
-                        # 实际也做对的情况
-                        if n.iloc[i][j] ==1:
-                            R1 += IL[i][l]
-                    # I0,R0的情况，表示理论上不能做对
-                    else:
-                        I0 += IL[i][l]
-                        # 实际却做对的情况
-                        if n.iloc[i][j] ==1:
-                            R0 += IL[i][l]
+                I1 += r.A[:,l]* IL[i][l]
+                R1 += r.A[:,l]* IL[i][l]*n[i]
+                I0 += (1-r.A[:,l])* IL[i][l]
+                R0 += (1-r.A[:,l])* IL[i][l]*n[i]
 
-            #针对每一道题目，根据I0,R0,I1,R1，来更新s和g，更新后的sg，又重新计算似然函数矩阵IL
-            if abs(R0 / I0 - sg[j][1])<threshold and abs((I1-R1) / I1 -sg[j][0]<threshold):
-                continueSG = False
-            sg[j][1] = R0 / I0
-            sg[j][0] = (I1-R1) / I1
-            print('-------------------------------')
-            print(str(j + 1) + '题目finish')
-
+        #针对每一道题目，根据I0,R0,I1,R1，来更新s和g，更新后的sg，又重新计算似然函数矩阵IL
+        # if (abs(R0 / I0 - sg[:,1])<threshold).any() and (abs((I1-R1) / I1 -sg[:,0])<threshold).any():
+        if kk ==2:
+            continueSG = False
+        sg[:,1] = R0 / I0
+        sg[:,0] = (I1-R1) / I1
         print(sg)
-        print(str(kk ) +"次迭代，"+str(ni)+"个学生，20道题目的失误率和猜测率")
+        print(str(kk ) +"次迭代，"+str(ni)+"个学生，"+str(nj)+"道题目的失误率和猜测率")
         kk +=1
     endTime = time.time()
-    print('********************************************')
     print('训练消耗时间：'+str(int(endTime-startTime))+'秒')
     print('********************************************')
-    return sg
-
+    return sg,r
 
 def trainIDINAModel():
     startTime = time.time()
@@ -251,7 +235,7 @@ def trainIDINAModel():
                             R0 += IL[i][l]
 
             #针对每一道题目，根据I0,R0,I1,R1，来更新s和g，更新后的sg，又重新计算似然函数矩阵IL
-            if abs(R0 / I0 - sg[j][1])<threshold and abs((I1-R1) / I1 -sg[j][0]<threshold):
+            if abs(R0 / I0 - sg[j][1])<threshold and abs((I1-R1) / I1 -sg[j][0])<threshold:
                 continueSG = False
             sg[j][1] = R0 / I0
             sg[j][0] = (I1-R1) / I1
@@ -270,78 +254,44 @@ def trainIDINAModel():
 
 
 def testPredict():
-    # 得到模型的参数，s和g
-    sg = trainIDINAModel()
+    # 得到模型的参数，s和g,
+    sg,r = trainDINAModel()
     # sg = trainDINAModel()
     startTime = time.time()
     print('预测开始时间：' + str(int(startTime)))
     # 测试集随机挑选20%的数据
-    # n = pd.read_csv('math2015/FrcSub/data.csv').sample(frac=0.2)
-    n = pd.read_csv('math2015/FrcSub/data.csv').head(headNum).sample(frac=0.2)
-    Q = pd.read_csv('math2015/FrcSub/q.csv')
+    n = pd.read_csv('math2015/Math1/data.csv').head(headNum).sample(frac=0.2).values
+    # n = pd.read_csv('math2015/FrcSub/data.csv').sample(frac=0.2).values
+    # Q = np.mat(pd.read_csv('math2015/FrcSub/q.csv'))
+    Q = np.mat(pd.read_csv('math2015/Math1/q.csv').head(15).values)
     # 536*0.2个学生,20道题目
     ni, nj = n.shape
     # 20道题目，8个知识点
     Qi, Qj = Q.shape
     # 预测的每个学生的技能向量
-    a = np.zeros(ni,dtype=int)
     IL = np.ones((ni, 2 ** Qj))
 
     for l in range(2 ** Qj):
         # 学生的数量
         for i in range(ni):
             # 题目的数量
-            for j in range(nj):
-                # 计算Q矩阵中，第k题的情况下，l这个技能模式能否答对
-                nl = nTrueOrFalse((Q.iloc[j]), l)
-                # 理论上，答对的情况
-                if nl == 1:
-                    # 实际该学生，对于k这道题做对了，似然函数*(1-s)
-                    if n.iloc[i][j] == 1:
-                        IL[i][l] *= (1 - sg[j][0])
-                    # 实际该学生，对于k这道题居然做错了，似然函数*s，失误率
-                    else:
-                        IL[i][l] *= sg[j][0]
-                # 理论上，答错的情况
-                else:
-                    # 实际该学生，对于k这道题居然做对了，似然函数*g，猜测率
-                    if n.iloc[i][j] == 1:
-                        IL[i][l] *= sg[j][1]
-                    # 实际该学生，对于k这道题确实做错了，似然函数*(1-g)
-                    else:
-                        IL[i][l] *= 1 - sg[j][1]
-
-
+            ll = map(lambda nl, nk, s, g: ((1 - s) ** nk * s ** (1 - nk)) ** nl * (g ** nk * (1 - g) ** (1 - nk)) ** (1 - nl), n[i], r.T[l].tolist()[0], sg[:, 0], sg[:, 1])
+            IL[i][l] = reduce(mul, ll)
     # 只需要在上面的IL中，针对每一个学生，寻找他在所有l模式中，似然函数最大的那一项l
-    for i in range(ni):
-        v_max = 0
-        v_max_index = 0
-        for l in range(2 ** Qj):
-            if IL[i][l] > v_max:
-                v_max = IL[i][l]
-                v_max_index = l
-        a[i]=v_max_index
-        #print(bin(v_max_index).replace('0b',''))
+    a = IL.argmax(axis=1)
     print('测试集中各个学生的预测的a技能向量')
     print(a)
 
-
     # 计算准确率
-
-    # 统计错误的数量
-    countErr = 0
-    for i in range(ni):
-        for k in range(nj):
-            # 预测的a值针对k这道题理论上的情况和实际如果不相同，累积+1
-            if nTrueOrFalse(Q.iloc[k],a[i])!=n.iloc[i][k]:
-                countErr +=1
-    print('总共有'+str(ni)+'人，正确率为')
-    print(1-countErr/(ni*nj))
+    i, j = n.shape
+    print('总共有'+str(ni)+'个人，准确率为：')
+    print(np.sum((r[:, a] == n.T) * 1) / (i * j))
     print('预测消耗时间：' + str(int(time.time())-int(startTime))+'秒')
-
 
 def main():
     testPredict()
+
+
 
 if __name__ == "__main__":
     main()
